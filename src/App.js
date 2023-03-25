b122910ab44ac6011a91431e4297ec93a3c4eb9b
Suresh <jsithsuresh@gmail.com>
2023-03-25 20:41:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouseChimney, faScrewdriverWrench, faBell, faUser, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
 function App() {
+   fetch('https://dev.deepthought.education/assets/uploads/files/files/others/ddugky_project.json')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header">
+          test
+          <img
+            src="https://deepthought.education/assets/images/logo/logo.svg"
+            alt="logo"
+          />
+          <button><FontAwesomeIcon icon={faHouseChimney} /></button>
+          <button><FontAwesomeIcon icon={faScrewdriverWrench} /></button>
+          <button><FontAwesomeIcon icon={faBell} /></button>
+          <button><FontAwesomeIcon icon={faUser} /></button>
+          <button><FontAwesomeIcon icon={faEllipsisVertical} /></button>
+        </div>
       </header>
     </div>
   );
